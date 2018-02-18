81a14491e37c3e93a4736de3cd9fac8154d78b88
Maximilian Schmidt <m.schmidt@obis-concept.de>
2018-02-18 23:27:17 +0100

    FIX: Checking for empty strings

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ module.exports = function isset(test,type) {
     _check = false;
   } else {
     _check = true;
+
     try {
       _instance = test instanceof type;
     } catch (e) {
@@ -16,6 +17,8 @@ module.exports = function isset(test,type) {
     test != null &&
     test != undefined && (
       !_check || (typeof test === type || _instance)
+    ) && (
+      !_check || !(type.toLowerCase() == 'string') || (test != '' && test != ' ')
     )
   )
 }
