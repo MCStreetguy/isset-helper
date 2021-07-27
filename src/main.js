ec6be75e5e8e5353d391f3c525e36cab886fff1c
Maximilian Schmidt <m.schmidt@obis-concept.de>
2021-07-27 19:51:16 +0200

    improving it some more

--- src/main.js
+++ src/main.js
@@ -27,11 +27,14 @@ function isset(test, type) {
   return (
     test != null &&
     test != undefined && (
-      // evaluates to true if no typecheck is required, the typecheck succeeded or the previous instanceof check succeeded
-      !_check || (typeof test === type || _instance)
-    ) && (
-      // evaluates to true if no typecheck is required, the requested type is not a string or the value is not an empty string
-      !_check || (type != 'string') || (typeof test === 'string' && test.trim().length > 0)
+      // skip all the typechecks entirely if not desired
+      !_check || (
+        // evaluates to true if the typecheck succeeded or the previous instanceof check succeeded
+        (typeof test === type || _instance)
+      ) && (
+        // evaluates to true if the requested type is not a string or the value is not an empty string
+        (type != 'string') || (typeof test === 'string' && test.trim().length > 0)
+      )
     )
   );
 }
