86923f6317a94f77fcd3089393214fff4aeb14bb
Maximilian Schmidt <m.schmidt@obis-concept.de>
2021-07-27 19:54:05 +0200

    add special edge-case check for arrays

--- src/main.js
+++ src/main.js
@@ -30,7 +30,10 @@ function isset(test, type) {
       // skip all the typechecks entirely if not desired
       !_check || (
         // evaluates to true if the typecheck succeeded or the previous instanceof check succeeded
-        (typeof test === type || _instance)
+        (typeof test === type || _instance) || (
+          // special edge-case check for type 'array' as these are technically of type 'object'
+          type === 'array' && Array.isArray(test) 
+        )
       ) && (
         // evaluates to true if the requested type is not a string or the value is not an empty string
         (type != 'string') || (typeof test === 'string' && test.trim().length > 0)
